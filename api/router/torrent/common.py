22fcc10ddf890c269a9c9f0e3432767f8eb1fb92
nitin <mneonizer@gmail.com>
2025-01-04 16:09:03 +0530

    updated logic for continuation of download from other user directories

--- api/router/torrent/common.py
+++ api/router/torrent/common.py
@@ -11,20 +11,26 @@ class MagnetDto(BaseModel):
 
 magnet_utils = MagnetUtils()
 
+def get_directory_size(directory):
+    return sum(f.stat().st_size for f in os.scandir(directory) if f.is_file())
+
 def copy_if_already_exists(info_hash, user_id):
     if redis.get(f"{user_id}/{info_hash}/copied_from_existing"):
         return
 
-    existing_save_dirs = glob.glob(f"/downloads/*/{info_hash}")
-    if len(existing_save_dirs) > 1:
-        for src_save_dir in existing_save_dirs:
-            if str(user_id) in src_save_dir:
-                continue
+    existing_directories = glob.glob(f"/downloads/*/{info_hash}")
+    current_user_directory = os.path.join(f"/downloads/{user_id}/{info_hash}")
 
-            dst_save_dir = os.path.join("/downloads", str(user_id), os.path.basename(src_save_dir))
-            os.makedirs(dst_save_dir, exist_ok=True)
-            shutil.copytree(src_save_dir, dst_save_dir, dirs_exist_ok=True)
-            redis.set(f"{user_id}/{info_hash}/copied_from_existing", 1)
+    if len(existing_directories) > 1:
+        try:
+            other_user_directories = [directory for directory in existing_directories if str(user_id) not in directory]
+            if other_user_directories:
+                # Find the directory with the largest size
+                largest_directory = max(other_user_directories, key=get_directory_size)
+                shutil.copytree(largest_directory, current_user_directory, dirs_exist_ok=True)
+                redis.set(f"{user_id}/{info_hash}/copied_from_existing", 1)
+        except Exception as error:
+            print(error)
 
 def update_to_db(props, user_id):
     if not user_id:
