d8a579bdedc9ce69a7bb5b9d1a083629350d58e2
nitin <mneonizer@gmail.com>
2025-01-12 20:27:52 +0530

    added socket signal for delete

--- api/router/torrent/delete.py
+++ api/router/torrent/delete.py
@@ -5,6 +5,7 @@ from ..auth.common import authenticate_user
 from bson import ObjectId
 from ..files.delete import delete_dir
 import asyncio
+from shared.sockets import emit
 
 router = APIRouter()
 
@@ -23,18 +24,20 @@ async def delete_torrent(request: DeleteTorrentRequest, request_obj: Request):
         if not torrent["is_finished"]:
             db.torrents.update_one({"_id": torrent["_id"]}, {"$set": {"is_paused": True}})
             redis.set(f"{user_id}/{torrent['info_hash']}/stop", 1)
-            
+
             # Check the status in the database every 1 second
             while True:
                 updated_torrent = await db.torrents.find_one({"_id": torrent["_id"]}, {"is_paused": True})
                 if updated_torrent["is_paused"]:
                     break
                 await asyncio.sleep(1)
-        
+
         # Delete the folder stored at save_dir
         delete_dir(torrent["save_dir"])
 
         db.torrents.delete_one({"_id": torrent["_id"]})
+
+        emit(f"/stc/torrent-added-or-removed", {"action": "removed", "info_hash": request.info_hash}, user_id)
         return {"message": "Torrent and associated files deleted successfully"}
     else:
         raise HTTPException(status_code=404, detail="Torrent not found")
