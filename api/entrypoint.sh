a072ed5173cc63659792f4491cf4f5c297e438a1
Nitin <mneonizer@gmail.com>
2026-02-02 10:51:01 +0530

    updated jackett config

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ ! -d "/app/jackett" ]; then
+    echo "Jackett directory not found, extracting from tar file..."
+    if [ -f "/app/jackett.tar" ]; then
+        tar -xf jackett.tar
+        echo "Extraction complete."
+    else
+        echo "Error: jackett.tar not found!"
+        exit 1
+    fi
+fi
+
 celery -A celery_worker worker -Q transcoding --concurrency=$CELERY_NUM_WORKERS --loglevel=info -E &
 
 if [ "$API_DEBUG" = "True" ]; then
