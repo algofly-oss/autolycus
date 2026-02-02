4dca9935b9e8f47a2ae95e614edc35909f44cfdd
Nitin <mneonizer@gmail.com>
2026-02-02 11:04:43 +0530

    Added automated certificate generation

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Configure Jackett Indexers
 if [ ! -d "/app/jackett" ]; then
     echo "Jackett directory not found, extracting from tar file..."
     if [ -f "/app/jackett.tar" ]; then
@@ -11,8 +12,21 @@ if [ ! -d "/app/jackett" ]; then
     fi
 fi
 
+# Configure SSL Certificate
+if [ ! -d "/app/certificate" ]; then
+    echo "Certificate directory not found, running generate_certificate.sh..."
+    if [ -f "/app/generate_certificate.sh" ]; then
+        bash ./generate_certificate.sh
+    else
+        echo "Error: generate_certificate.sh not found!"
+        exit 1
+    fi
+fi
+
+# Start Celery Worker
 celery -A celery_worker worker -Q transcoding --concurrency=$CELERY_NUM_WORKERS --loglevel=info -E &
 
+# Start API Server
 if [ "$API_DEBUG" = "True" ]; then
     echo "Running Development Server"
     uvicorn main:app --reload --host 0.0.0.0 --port 8080
